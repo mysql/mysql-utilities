3aa34c3c767e4343643597512f177ab1b99a1421
Mats Kindahl <mats.kindahl@oracle.com>
2010-08-18 15:51:26 +0200

    WL#5528: Command-line utilities for MySQL
    Fixing so that cx_Freeze is used to build the package, which will allow Windows MSI packages to be constructed.

--- setup.py
+++ setup.py
@@ -1,4 +1,32 @@
-import distutils.core, unittest, os
+import sys
+import os
+import distutils.core
+import cx_Freeze
+
+META_INFO = {
+    'name':             'mysql-utilities',
+    'description':      'MySQL Command-line Utilities',
+    'maintainer':       'MySQL',         # !!!
+    'maintainer_email': "internals@lists.mysql.com", # !!!
+    'version':          '0.1.0',
+    'url':              'http://launchpad.net/???', # !!! Launchpad URL
+    'classifiers': [
+        'Programming Language :: Python',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: OS Independent',
+        'Operating System :: POSIX',
+        ],
+    }
+
+INSTALL = {
+    'packages': ["mysql"],
+    'scripts': [
+        'scripts/mysqlproc.py'
+        ],
+    }
 
 class TestCommand(distutils.core.Command):
     user_options = [ ]
@@ -7,7 +35,7 @@ class TestCommand(distutils.core.Command):
         self._dir = os.getcwd()
         # Install mock database
         # @todo add option to use real database later
-        import sys, tests.MySQLdb
+        import tests.MySQLdb
         sys.modules['MySQLdb'] = tests.MySQLdb
 
     def finalize_options(self):
@@ -16,24 +44,22 @@ class TestCommand(distutils.core.Command):
     def run(self):
         "Finds all the tests modules in tests/, and runs them."
 
+        import unittest
         import tests.command
         suite = unittest.TestSuite()
         suite.addTest(tests.command.suite())
         runner = unittest.TextTestRunner(verbosity=1)
         runner.run(suite)
 
-distutils.core.setup(
-    name='mysql-utilities', description='MySQL Command-line Utilities',
-    maintainer="MySQL",         # !!!
-    maintainer_email="internals@lists.mysql.com", # !!!
-    version='0.1.0',
-    url='http://launchpad.net/???', # !!! Launchpad URL
-    packages=[ 'mysql' ],
-    scripts=[
-        'scripts/mysqlproc',
-    ],
-    classifiers=[
-        'Programming Language :: Python',
-    ],
-    cmdclass = { 'test': TestCommand },
-)
+COMMANDS = {
+    'cmdclass': { 'test': TestCommand },
+    }
+
+ARGS = {
+    'executables': [cx_Freeze.Executable(exe) for exe in INSTALL['scripts']],
+    }
+    
+ARGS.update(META_INFO)
+ARGS.update(INSTALL)
+ARGS.update(COMMANDS)
+cx_Freeze.setup(**ARGS)
