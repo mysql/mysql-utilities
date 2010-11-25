6c767e49fbf8d77e475b55a41a9a1a585e551707
Mats Kindahl <mats.kindahl@oracle.com>
2010-11-25 11:53:26 +0100

    Moving meta-information into separate file.
    Separating freezing script from normal setup
    script.
    
    Disabling Sphinx build if Sphinx is not available.
    
    Adding requires information for Sphinx (1.0
    is required because man page builder was added
    then).
    
    Adding stub to automatically download setuptools
    if not available.
    
    Do not automatically build manuals when building:
    they have to be built separately now.

--- setup.py
+++ setup.py
@@ -1,73 +1,23 @@
+#!/usr/bin/python
+
+# Boilerplate code to install setuptools if it is not installed
+import ez_setup
+ez_setup.use_setuptools()
+
 # Keep the imports sorted (alphabetically) in each group. Makes
 # merging easier.
 
+import distutils.core
 import glob
 import os
 import setuptools
 import sys
 
-from distutils import log
-from distutils.command.build_scripts import build_scripts
-from distutils.core import Command
-from distutils.util import convert_path
-from sphinx.setup_command import BuildDoc
-
-import distutils.core
-
 import mysql.utilities
 
-META_INFO = {
-    'description':      'MySQL Utilities',
-    'maintainer':       'MySQL Utilities Team',
-    'maintainer_email': "internals@lists.mysql.com", # !!!
-    'version':          mysql.utilities.VERSION_STRING,
-    'url':              'http://launchpad.net/???', # !!! Launchpad URL
-    'classifiers': [
-        'Development Status :: 3 - Alpha',
-        'Programming Language :: Python :: 2.6',
-        'Environment :: Console',
-        'Environment :: Win32 (MS Windows)',
-        'License :: OSI Approved :: GNU General Public License (GPL)',
-        'Intended Audience :: Developers',
-        'Intended Audience :: System Administrators',
-        'Intended Audience :: Database Administrators',
-        'Operating System :: Microsoft :: Windows',
-        'Operating System :: OS Independent',
-        'Operating System :: POSIX',
-        'Topic :: Utilities',
-        ],
-    'setup_requires': [
-        'Sphinx',
-        ],
-    'install_requires': [
-        'MySQLdb',
-        ],
-    }
+from info import META_INFO, INSTALL, COMMANDS
 
-INSTALL = {
-    'packages': setuptools.find_packages(exclude=["tests"]),
-    'scripts': glob.glob('scripts/*.py'),
-    }
-
-ARGS = {
-    'test_suite': 'tests.test_all',
-}
-
-if sys.platform.startswith("win32"):
-    from cx_Freeze import setup, Executable
-    META_INFO['name'] = 'MySQL Utilities'
-    ARGS.update({
-            'executable': [
-                Executable(exe, base="Console") for exe in INSTALL['scripts']
-                ],
-            'options': { 'bdist_msi': { 'add_to_path': True, },
-                }
-            })
-else:
-    from setuptools import setup
-    META_INFO['name'] = 'mysql-utilities'
-
-class install_man(Command):
+class install_man(distutils.core.Command):
     description = "install (Unix) manual pages"
 
     user_options = [
@@ -89,7 +39,6 @@ class install_man(Command):
         self.set_undefined_options('install',
                                    ('install_data', 'install_base'),
                                    ('force', 'force'),
-                                   ('skip_build', 'skip_build'),
                                    )
         self.set_undefined_options('build_sphinx',
                                    ('build_dir', 'build_dir'),
@@ -98,75 +47,45 @@ class install_man(Command):
         self.source_dir = os.path.join(self.build_dir, 'man')
 
     def run(self):
-        if not self.skip_build:
-            self.run_command('build_man')
-        for man_file in glob.glob(os.path.join(self.source_dir, '*.[12345678]')):
+        for man_file in self.get_outputs():
             man_dir = 'man' + os.path.splitext(man_file)[1][1:]
             man_page = os.path.basename(man_file)
             self.mkpath(man_dir)
-            self.copy_file(man_file, os.path.join(self.target_dir, man_dir, man_page))
+            man_install = os.path.join(self.target_dir, man_dir, man_page)
+            self.copy_file(man_file, man_install)
 
-class MyBuildScripts(build_scripts):
-    """Class for providing a customized version of build_scripts.
+    def get_outputs(self):
+        return glob.glob(os.path.join(self.source_dir, '*.[12345678]'))
 
-    When ``run`` is called, this command class will:
-    1. Create a copy of all ``.py`` files in the **scripts** option
-       that does not have the ``.py`` extension.
-    2. Replace the list in the **scripts** attribute with a list
-       consisting of the script files with the ``.py`` extension
-       removed.
-    3. Call run method in `distutils.command.build_scripts`.
-    4. Restore the scripts list to the old value, for other commands
-       to use."""
-
-    def run(self):
-        if not self.scripts:
-            return
-        saved_scripts = self.scripts
-        self.scripts = []
-        for script in saved_scripts:
-            script = convert_path(script)
-            script_copy, script_ext = os.path.splitext(script)
-
-            if script_ext != '.py':
-                log.debug("Not removing extension from %s since it's not '.py'", script)
-            else:
-                log.debug("Copying %s -> %s", script, script_copy)
-                self.copy_file(script, script_copy)
-                self.scripts.append(script_copy)
-        # distutils is compatible with 2.1 so we cannot use super() to
-        # call it.
-        build_scripts.run(self)
-        self.scripts = saved_scripts
-
-
-COMMANDS = {
-    'cmdclass': {
-        'build_scripts': MyBuildScripts,
-        },
-    }
-
-if not sys.platform.startswith("win32"):
+# See if we have Sphinx installed, otherwise, just ignore building
+# documentation.
+try:
+    import sphinx.setup_command
     from distutils.command.install import install
-    from distutils.command.build import build
 
     class MyInstall(install):
-        sub_commands = install.sub_commands + [('install_man', lambda self: True)]
-
-    class MyBuild(build):
-        sub_commands = build.sub_commands + [('build_man', lambda self: True)]
+        sub_commands = install.sub_commands + [
+            ('install_man', lambda self: True),
+            ]
 
     COMMANDS['cmdclass'].update({
             'install': MyInstall,
-            'build': MyBuild,
-            'build_man': BuildDoc,
             'install_man': install_man,
+            'build_man': sphinx.setup_command.BuildDoc,
             })
+
     COMMANDS.setdefault('options',{}).update({
             'build_man': { 'builder': 'man' },
             })
+except ImportError:
+    # TODO: install pre-generated manual pages
+    pass                        # No Sphinx installed
+
+ARGS = {
+    'test_suite': 'tests.test_all',
+}
 
 ARGS.update(META_INFO)
 ARGS.update(INSTALL)
 ARGS.update(COMMANDS)
-setup(**ARGS)
+setuptools.setup(**ARGS)
