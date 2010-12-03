4fc2e667a97c965363264638f6b4f83c69b5a8cd
Chuck Bell <chuck.bell@oracle.com>
2010-12-03 13:46:29 -0500

    WL#5586 : mysqldbexport
    Fixed issue with --skip options. 
    Added test for skips.

--- mysql/utilities/command/dbexport.py
+++ mysql/utilities/command/dbexport.py
@@ -19,7 +19,7 @@
 """
 This file contains the export operations that will export object metadata or
 table data.
-""" 
+"""
 
 import re
 import sys
@@ -27,12 +27,12 @@ from mysql.utilities.exception import MySQLUtilError
 
 def export_metadata(src_val, db_list, options):
     """Produce rows to be used to recreate objects in a database.
-    
+
     This method retrieves the objects for each database listed in the form
     of CREATE (SQL) statements or in a tabular form to the file specified.
     The valid values for the format parameter are SQL, CSV, TSV, VERTICAL,
-    or GRID. 
-    
+    or GRID.
+
     src_val[in]        a dictionary containing connection information for the
                        source including:
                        (user, password, host, port, socket)
@@ -43,18 +43,25 @@ def export_metadata(src_val, db_list, options):
 
     Returns bool True = success, False = error
     """
-    
+
     from mysql.utilities.common.database import Database
     from mysql.utilities.common.server import connect_servers
     from mysql.utilities.common.format import format_tabular_list
     from mysql.utilities.common.format import format_vertical_list
-    
+
     format = options.get("format", "SQL")
     no_headers = options.get("no_headers", False)
     column_type = options.get("display", "BRIEF")
     skip_create = options.get("skip_create", False)
     silent = options.get("silent", False)
-    
+    skip_tables = options.get("skip_tables", False)
+    skip_views = options.get("skip_views", False)
+    skip_triggers = options.get("skip_triggers", False)
+    skip_procs = options.get("skip_procs", False)
+    skip_funcs = options.get("skip_funcs", False)
+    skip_events = options.get("skip_events", False)
+    skip_grants = options.get("skip_grants", False)
+
     try:
         servers = connect_servers(src_val, None, silent, "5.1.30")
         #print servers
@@ -68,19 +75,16 @@ def export_metadata(src_val, db_list, options):
         try:
             source_db = Database(source, db_name)
             source_db.check_read_access(src_val["user"], src_val["host"],
-                                        options.get("skip_views", False),
-                                        options.get("skip_procs", False),
-                                        options.get("skip_funcs", False),
-                                        options.get("skip_grants", False),
-                                        options.get("skip_events", False))
+                                        skip_views, skip_procs, skip_funcs,
+                                        skip_grants, skip_events)
         except MySQLUtilError, e:
             raise e
-            
+
     for db_name in db_list:
-        
+
         # Get a Database class instance
         db = Database(source, db_name, options)
-        
+
         # Error is source database does not exist
         if not db.exists():
             raise MySQLUtilError("Source database does not exist - %s" %
@@ -88,7 +92,7 @@ def export_metadata(src_val, db_list, options):
 
         if not silent:
             print "# Exporting metadata from %s" % db_name
-        
+
         # Perform the extraction
         try:
             if format == "SQL":
@@ -98,7 +102,7 @@ def export_metadata(src_val, db_list, options):
                     print "CREATE DATABASE %s;" % db_name
                 print "USE %s;" % db_name
                 for dbobj in db.get_next_object():
-                    if dbobj[0] == "GRANT":
+                    if dbobj[0] == "GRANT" and not skip_grants:
                         if not silent:
                             print "# Grant:"
                         if dbobj[1][3]:
@@ -115,23 +119,36 @@ def export_metadata(src_val, db_list, options):
                         if not silent:
                             print "# %s: %s.%s" % (dbobj[0], db_name,
                                                    dbobj[1][0])
-                        if dbobj[0] == "PROCEDURE" or \
-                           dbobj[0] == "FUNCTION" or \
-                           dbobj[0] == "EVENT" or \
-                           dbobj[0] == "TRIGGER":
+                        if (dbobj[0] == "PROCEDURE" and not skip_procs) or \
+                           (dbobj[0] == "FUNCTION" and not skip_funcs) or \
+                           (dbobj[0] == "EVENT" and not skip_events) or \
+                           (dbobj[0] == "TRIGGER" and not skip_triggers):
                             print "DELIMITER ||"
                         print "%s;" % db.get_create_statement(db_name,
                                                               dbobj[1][0],
                                                               dbobj[0])
-                        if dbobj[0] == "PROCEDURE" or \
-                           dbobj[0] == "FUNCTION" or \
-                           dbobj[0] == "EVENT" or \
-                           dbobj[0] == "TRIGGER":
+                        if (dbobj[0] == "PROCEDURE" and not skip_procs) or \
+                           (dbobj[0] == "FUNCTION" and not skip_funcs) or \
+                           (dbobj[0] == "EVENT" and not skip_events) or \
+                           (dbobj[0] == "TRIGGER" and not skip_triggers):
                             print "||"
                             print "DELIMITER ;"
             else:
-                objects = ["TABLE", "VIEW", "TRIGGER", "PROCEDURE",
-                           "FUNCTION", "EVENT", "GRANT"]
+                objects = []
+                if not skip_tables:
+                    objects.append("TABLE")
+                if not skip_views:
+                    objects.append("VIEW")
+                if not skip_triggers:
+                    objects.append("TRIGGER")
+                if not skip_procs:
+                    objects.append("PROCEDURE")
+                if not skip_funcs:
+                    objects.append("FUNCTION")
+                if not skip_events:
+                    objects.append("EVENT")
+                if not skip_grants:
+                    objects.append("GRANT")
                 for obj_type in objects:
                     sys.stdout.write("# %sS in %s:" % (obj_type, db_name))
                     rows = db.get_db_objects(obj_type, column_type, True)
@@ -153,7 +170,7 @@ def export_metadata(src_val, db_list, options):
 
         except MySQLUtilError, e:
             raise e
-            
+
     if not silent:
         print "#...done."
     return True
@@ -163,7 +180,7 @@ def _export_row(data_rows, cur_table, col_metadata,
                 format, single, skip_blobs, first=False,
                 no_headers=False, outfile=None):
     """Export a row
-    
+
     This method will print a row to stdout based on the format chosen -
     either SQL statements, GRID, CSV, TSV, or VERTICAL.
 
@@ -210,7 +227,7 @@ def _export_row(data_rows, cur_table, col_metadata,
                                                     col_metadata)
             rows = data_lists[0]
             blob_rows = data_lists[1]
-            
+
             if len(rows) > 0:
                 for row in rows:
                     if outfile is not None:
@@ -219,7 +236,7 @@ def _export_row(data_rows, cur_table, col_metadata,
                         print "%s;" % row
             else:
                 print "# Table %s has no data." % tbl_name
-                
+
         if len(blob_rows) > 0:
             if skip_blobs:
                 print "# WARNING : Table %s has blob data that " \
@@ -249,12 +266,12 @@ def _export_row(data_rows, cur_table, col_metadata,
 
 def export_data(src_val, db_list, options):
     """Produce data for the tables in a database.
-    
+
     This method retrieves the data for each table in the databases listed in
     the form of BULK INSERT (SQL) statements or in a tabular form to the file
     specified. The valid values for the format parameter are SQL, CSV, TSV,
-    VERITCAL, or GRID. 
-    
+    VERITCAL, or GRID.
+
     src_val[in]        a dictionary containing connection information for the
                        source including:
                        (user, password, host, port, socket)
@@ -266,11 +283,11 @@ def export_data(src_val, db_list, options):
 
     Returns bool True = success, False = error
     """
-    
+
     from mysql.utilities.common.database import Database
     from mysql.utilities.common.table import Table
     from mysql.utilities.common.server import connect_servers
-    
+
     format = options.get("format", "SQL")
     no_headers = options.get("no_headers", True)
     column_type = options.get("display", "BRIEF")
@@ -278,7 +295,7 @@ def export_data(src_val, db_list, options):
     skip_blobs = options.get("skip_blobs", False)
     silent = options.get("silent", False)
     file_per_table = options.get("file_per_tbl", False)
-    
+
     try:
         servers = connect_servers(src_val, None, silent, "5.1.30")
     except MySQLUtilError, e:
@@ -298,12 +315,12 @@ def export_data(src_val, db_list, options):
                                         options.get("skip_events", False))
         except MySQLUtilError, e:
             raise e
-            
+
     for db_name in db_list:
-        
+
         # Get a Database class instance
         db = Database(source, db_name, options)
-        
+
         # Error is source database does not exist
         if not db.exists():
             raise MySQLUtilError("Source database does not exist - %s" %
@@ -315,7 +332,7 @@ def export_data(src_val, db_list, options):
             print "# Exporting data from %s" % db_name
             if file_per_table:
                 print "# Writing table data to files."
-        
+
         # Perform the extraction
         try:
             tables = db.get_db_objects("TABLE")
@@ -360,7 +377,7 @@ def export_data(src_val, db_list, options):
 
         except MySQLUtilError, e:
             raise e
-            
+
     if not silent:
         print "#...done."
     return True
