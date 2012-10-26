a1cc88d8d065a90e884fce6e52a9c44e0820ca6b
chuck.bell@oracle.com <>
2012-10-26 16:40:33 -0400

    SR#3-6287109261 : mysqldbcompare fails on server versions without '-'
    The code incorrectly attempts to split the version string based on
    a dash in the version number. But some releases do not have this 
    format. This patch changes the string parsing to a regex match.

--- mysql/utilities/common/server.py
+++ mysql/utilities/common/server.py
@@ -642,22 +642,13 @@ class Server(object):
         """
         version_str = self.get_version()
         if version_str is not None:
-            index = version_str.find("-")
-            if index >= 0:
-                parts = version_str[0:index].split(".")
+            match = re.match(r'^(\d+\.\d+(\.\d)*).*$', version_str.strip())
+            if match:
+                version = [int(x) for x in match.group(1).split('.')]
+                version = (version + [0])[:3]  # Ensure a 3 elements list
+                return version >= [int(t_major), int(t_minor), int(t_rel)]
             else:
-                parts = version_str.split(".")
-            major = parts[0]
-            minor = parts[1]
-            rel = parts[2]
-            if int(t_major) > int(major):
                 return False
-            elif int(t_major) == int(major):
-                if int(t_minor) > int(minor):
-                    return False
-                elif int(t_minor) == int(minor):
-                    if int(t_rel) > int(rel):
-                        return False
         return True
 
 
