c74503cd7f2062856a4e6ad8c4cd265441bf69d4
chuck.bell@oracle.com <>
2013-01-25 15:16:32 -0500

    WL#6221 : Utilities packaging
    This patch reverses the changes to the setup.py file so that utilities
    can be packaged with Workbench. The changes can be restored once the
    packaging work is complete.

--- setup.py
+++ setup.py
@@ -6,9 +6,6 @@
 import distutils.core
 import os
 import sys
-import re
-import subprocess
-from glob import glob
 
 # Setup function to use
 from distutils.core import setup
@@ -16,13 +13,6 @@ from distutils.core import setup
 from distutils.command.build_scripts import build_scripts as _build_scripts
 from distutils.command.install import install as _install
 from distutils.command.install_scripts import install_scripts as _install_scripts
-
-from distutils.file_util import copy_file, write_file
-from distutils.dir_util import remove_tree
-from distutils.filelist import findall
-from distutils.core import Command
-from distutils import log, dir_util
-
 from info import META_INFO, INSTALL
 
 COMMANDS = {
@@ -53,11 +43,11 @@ class install_scripts(_install_scripts):
 
     user_options = _install_scripts.user_options + [
         ("skip-profile", None, "Skip installing a profile script"),
-        ('root=', None,
-         "install everything relative to this alternate root directory"),
         ]
 
     boolean_options = _install_scripts.boolean_options + ['skip-profile']
+
+    profile_file = "/etc/profile.d/mysql-utilities.sh"
 
     def initialize_options(self):
         _install_scripts.initialize_options(self)
@@ -68,75 +58,29 @@ class install_scripts(_install_scripts):
         self.set_undefined_options('install',
                                    ('skip_profile', 'skip_profile'))
 
-        self.profile_filename = 'mysql-utilities.sh'
-        self.profile_file = os.path.join(self.build_dir, self.profile_filename)
-
-    def _create_shell_profile(self):
-        """Creates and installes the shell profile
-        """
-        if self.skip_profile:
-            log.info("Not adding shell profile %s (skipped)" % (
-                     os.path.join(install_dir, self.profile_filename)))
-            return
-
-        # When creating an RPM, we need to fix the location of scripts
-        building_rpm = False
-        if 'BUILDROOT' in self.install_dir:
-            building_rpm = True
-            install_dir = re.search('BUILDROOT/.*?(/.*)$',
-                                    self.install_dir).group(1)
-        else:
-            install_dir = '/etc/profile.d'
-            destfile = os.path.join(install_dir, self.profile_filename)
-            if not os.access(destfile, os.X_OK | os.W_OK):
-                log.info("Not installing mysql-utilities.sh in "
-                         "%s (no permission)" % install_dir)
-                self.skip_profile = True
-                return
-
-        if os.path.exists(os.path.dirname(self.profile_file)):
-            outfile = self.profile_file
-            if os.path.isdir(outfile) and not os.path.islink(outfile):
-                dir_util.remove_tree(outfile)
-            elif os.path.exists(outfile):
-                log.info("Removing %s", outfile)
-                os.unlink(outfile)
-
-            script = PROFILE_SCRIPT % (install_dir,)
-            log.info("Writing %s", outfile)
-            open(outfile, "w+").write(script)
-
-        if not building_rpm:
-            self.copy_file(outfile, install_dir)
-
-    def _create_batch_file(self, script):
-        """Create Batch files to make scripts executable on MSWindows
-        """
-        if os.name != 'nt':
-            return
-        scriptname = os.path.basename(script)
-        log.info("creating Batch executable for %s", scriptname)
-        batfile = script + '.bat'
-        scriptloc = os.path.join(sys.exec_prefix, 'Scripts', scriptname)
-        fp = open(batfile, 'w')
-        fp.write("@{0} {1} %*".format(sys.executable, scriptloc))
-        fp.close()
-        #self.outfiles.append(batfile)
-
     def run(self):
+        from distutils import log, dir_util
         # We should probably use distutils.dist.execute here to allow
         # --dry-run to work properly.
-        self._create_shell_profile()
+        if not self.skip_profile:
+            if os.path.exists(os.path.dirname(self.profile_file)):
+                outfile = self.profile_file
+                if os.path.isdir(outfile) and not os.path.islink(outfile):
+                    dir_util.remove_tree(outfile)
+                elif os.path.exists(outfile):
+                    log.info("Removing %s", outfile)
+                    os.unlink(outfile)
+                script = PROFILE_SCRIPT % (self.install_dir,)
+                log.info("Writing %s", outfile)
+                open(outfile, "w+").write(script)
+            else:
+                log.info("Not adding %s%s", self.profile_file,
+                         " (skipped)" if self.skip_profile else "")
         _install_scripts.run(self)
-        
-        # self.outfiles will be updated by the for-loop
-        scripts = self.get_outputs()
-        for script in scripts:
-            self._create_batch_file(script)
 
     def get_outputs(self):
         outputs = _install_scripts.get_outputs(self)
-        if not self.skip_profile and os.name != 'nt':
+        if not self.skip_profile:
             outputs.append(self.profile_file)
         return outputs
 
@@ -148,10 +92,6 @@ class install_man(distutils.core.Command):
         ('force', 'f', 'force installation (overwrite existing files)'),
         ('build-dir=', 'b', 'Build directory'),
         ('skip-build', None, "skip the build steps"),
-        ('record=', None,
-         "filename in which to record list of installed files"),
-        ('root=', None,
-         "install everything relative to this alternate root directory"),
     ]
 
     boolean_options = ['force']
@@ -161,31 +101,22 @@ class install_man(distutils.core.Command):
         self.build_dir = None
         self.force = None
         self.skip_build = None
-        self.record = None
-        self.root = None
 
     def finalize_options(self):
         self.set_undefined_options('install',
                                    ('install_data', 'install_base'),
                                    ('force', 'force'),
-                                   ('root', 'root'),
                                    )
         self.set_undefined_options('build_sphinx',
                                    ('build_dir', 'build_dir'),
                                    )
-
-        if self.root:
-            # Convert absolute to relative
-            if self.install_base[0] == '/':
-                self.target_dir = os.path.join(self.root, self.install_base[1:])
-            else:
-                self.target_dir = os.path.join(self.root, self.install_base)
-        else:
-            self.target_dir = os.path.join(self.install_base, 'man')
-
+        self.target_dir = os.path.join(self.install_base, 'man')
         self.source_dir = os.path.join(self.build_dir, 'man')
 
     def run(self):
+        from glob import glob
+        from distutils import log
+
         outfiles = []
         man_files = glob(os.path.join(self.source_dir, '*.[12345678]'))
         for man_file in man_files:
@@ -195,20 +126,7 @@ class install_man(distutils.core.Command):
             man_install = os.path.join(self.target_dir, man_dir, man_page)
             self.copy_file(man_file, man_install)
             outfiles.append(man_install)
-            print "man_install:", man_install
         self.outfiles = outfiles
-
-        # write list of installed files, if requested.
-        if self.record:
-            outputs = self.get_outputs()
-            if self.root:               # strip any package prefix
-                root_len = len(self.root)
-                for counter in xrange(len(outputs)):
-                    outputs[counter] = outputs[counter][root_len:]
-            self.execute(write_file,
-                         (self.record, outputs),
-                         "writing list of installed files to '%s'" %
-                         self.record)
 
     def get_outputs(self):
         return self.outfiles or []
@@ -258,20 +176,19 @@ class build_scripts(_build_scripts):
 
     When ``run`` is called, this command class will:
     1. Create a copy of all ``.py`` files in the **scripts** option
-       that does not have the ``.py`` extension. On Windows, also
-       create a Batch file which executes the Python script.
+       that does not have the ``.py`` extension.
     2. Replace the list in the **scripts** attribute with a list
        consisting of the script files with the ``.py`` extension
        removed.
     3. Call run method in `distutils.command.build_scripts`.
     4. Restore the scripts list to the old value, for other commands
-       to use.
-    """
+       to use."""
 
     def run(self):
+        from distutils import log
+
         if not self.scripts:
             return
-
 
         saved_scripts = self.scripts
         self.scripts = []
@@ -281,127 +198,19 @@ class build_scripts(_build_scripts):
 
             if script_ext != '.py':
                 log.debug("Not removing extension from %s since it's not '.py'", script)
-                continue
-
-            log.debug("Copying %s -> %s", script, script_copy)
-            self.copy_file(script, script_copy)
-            self.scripts.append(script_copy)
-
+            else:
+                log.debug("Copying %s -> %s", script, script_copy)
+                self.copy_file(script, script_copy)
+                self.scripts.append(script_copy)
         # distutils is compatible with 2.1 so we cannot use super() to
         # call it.
         _build_scripts.run(self)
         self.scripts = saved_scripts
 
-class bdist_rpm(Command):
-    """Create a RPM distribution"""
-    description = 'create a source RPM distribution'
-    user_options = [
-        ('bdist-base=', 'd',
-         "base directory for creating built distributions"),
-        ('rpm-base=', 'd',
-         "base directory for creating RPMs (default <bdist-dir>/rpm)"),
-        ('keep-temp', 'k',
-         "keep the pseudo-installation tree around after "
-         "creating the distribution archive"),
-        ('dist-dir=', 'd',
-         "directory to put final built distributions in"),
-    ]
-
-    def initialize_options(self):
-        """Initialize the options"""
-        self.bdist_base = None
-        self.rpm_base = None
-        self.keep_temp = 0
-        self.dist_dir = None
-    
-    def finalize_options(self):
-        """Finalize the options"""
-        self.set_undefined_options('bdist',
-                                   ('bdist_base', 'bdist_base'))
-
-        if not self.rpm_base:
-            self.rpm_base = os.path.join(self.bdist_base, 'rpm')
-
-        self.rpm_spec = 'support/RPM/mysql_utilities.spec'
-
-        self.set_undefined_options('bdist', ('dist_dir', 'dist_dir'))
-
-    def _populate_rpmbase(self):
-        """Create and populate the RPM base directory"""
-        #self.mkpath(self.rpm_base)
-        self._rpm_dirs = {}
-        dirs = ['BUILD', 'RPMS', 'SOURCES', 'SPECS', 'SRPMS']
-        for dirname in dirs:
-            self._rpm_dirs[dirname] = os.path.join(self.rpm_base, dirname)
-            self.mkpath(self._rpm_dirs[dirname])
-    
-    def _prepare_distribution(self):
-        raise NotImplemented
-
-    def _create_rpm(self):
-        log.info("creating RPM using rpmbuild")
-
-        cmd = ['rpmbuild',
-            '-bb',
-            '-D', "_topdir " + os.path.abspath(self.rpm_base),
-            '-D', "release_info " + META_INFO['description'],
-            '-D', "version " + META_INFO['version'],
-            self.rpm_spec
-            ]
-        if not self.verbose:
-           cmd.append('--quiet')
-
-        self.spawn(cmd)
-        print ' '.join(cmd)
-
-        rpms = os.path.join(self.rpm_base, 'RPMS')
-        for base, dirs, files in os.walk(rpms):
-            for filename in files:
-                if filename.endswith('.rpm'):
-                    filepath = os.path.join(base, filename)
-                    copy_file(filepath, self.dist_dir)
-
-    def run(self):
-        """Run the distutils command"""
-        # check whether we can execute rpmbuild
-        if not self.dry_run:
-            try:
-                devnull = open(os.devnull, 'w')
-                subprocess.Popen('rpmbuild', stdin=devnull, stdout=devnull)
-            except OSError:
-                raise DistutilsError("Cound not execute rpmbuild. Make sure "
-                                     "it is installed and in your PATH")
-
-        self._populate_rpmbase()
-        
-        saved_dist_files = self.distribution.dist_files[:]
-        sdist = self.reinitialize_command('sdist')
-        sdist.formats = ['gztar']
-        self.run_command('sdist')
-        self.distribution.dist_files = saved_dist_files
-
-        source = sdist.get_archive_files()[0]
-        source_dir = self._rpm_dirs['SOURCES']
-        self.copy_file(source, source_dir)
-        
-        self._create_rpm()
-
-        if not self.keep_temp:
-            remove_tree(self.bdist_base, dry_run=self.dry_run)
-
 if os.name != "nt":
     COMMANDS['cmdclass'].update({
         'build_scripts': build_scripts,
         'install_scripts': install_scripts,
-        'bdist_rpm': bdist_rpm,
-        })
-if os.name == "nt" and sys.version_info >= (2, 7):
-    # MSI only supported for Python 2.7 and greater
-    from support import dist_msi
-    COMMANDS['cmdclass'].update({
-        'build_scripts': build_scripts,
-        'install_scripts': install_scripts,
-        'sdist_msi': dist_msi.SourceMSI,
         })
 
 ARGS.update(META_INFO)
