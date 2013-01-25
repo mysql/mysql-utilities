8f9b79f860416f9ba7c24d29c974871ab00db629
chuck.bell@oracle.com <>
2013-01-25 14:57:14 -0500

    Version and change log update for release-1.1.2

--- mysql/utilities/__init__.py
+++ mysql/utilities/__init__.py
@@ -1,8 +1,8 @@
 # Major, Minor, Patch, Status
-VERSION = (1, 1, 1, "", 5, 2, 45)
+VERSION = (1, 1, 2, "", 5, 2, 46)
 VERSION_STRING = "%s.%s.%s%s - MySQL Workbench Distribution %s.%s.%s" % VERSION
 RELEASE_STRING = "%s.%s.%s%s - MySQL Workbench Distribution %s.%s.%s" % VERSION
-COPYRIGHT = "2010, 2012 Oracle and/or its affiliates. All rights reserved."
+COPYRIGHT = "2010, 2013 Oracle and/or its affiliates. All rights reserved."
 COPYRIGHT_FULL = "Copyright (c) " + COPYRIGHT + """
 This program is free software; see the source for copying
 conditions. There is NO warranty; not even for MERCHANTABILITY or
