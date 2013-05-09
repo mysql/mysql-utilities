5e7e0f08e4ce7c5efa1d1c2103632770bbee202c
chuck.bell@oracle.com <>
2013-05-09 14:22:10 -0400

    BUG#16664899 : add external script return code checking
    Adding missing file.

---
+++ mysql-test/std_data/check_threshold.sh
@@ -0,0 +1,7 @@
+echo "# ARGUMENTS PASSED:"
+echo "# old master host: " $1
+echo "# old master port: " $2
+echo "# new master host: " $3
+echo "# new master port: " $4
+echo "# Returning code 10"
+exit 10
