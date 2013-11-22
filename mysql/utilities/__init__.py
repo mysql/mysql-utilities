2a9759f2758bc3929b9dbfce576d5427ae003d3b
Israel Gomez <israel.gomez@oracle.com>
2013-11-22 17:38:57 -0600

    Set initial version for tree.

--- mysql/utilities/__init__.py
+++ mysql/utilities/__init__.py
@@ -16,9 +16,9 @@
 #
 
 # Major, Minor, Patch, Status
-VERSION = (1, 4, 0, 'a', 0)
+VERSION = (1, 4, 1, 'a', 0)
 # Future versions will have to include only the X, Y (no Z).
-WORKBENCH_VERSION = (5, 2, 47)
+WORKBENCH_VERSION = (6, 0, 0)
 
 VERSION_STRING = "%s.%s.%s" % VERSION[0:3]
 RELEASE_STRING = (
