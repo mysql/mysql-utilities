ef92646897d74aa3016d1f6fb6b7cb50450490bc
chuck.bell@oracle.com <>
2013-12-16 10:49:49 -0500

    Initial repo: version set, tag moved.

--- mysql/utilities/__init__.py
+++ mysql/utilities/__init__.py
@@ -18,7 +18,7 @@
 """mysql.utilities"""
 
 # Major, Minor, Patch, Status
-VERSION = (1, 3, 6, 'GA', 0)
+VERSION = (1, 3, 7, 'GA', 0)
 # Future versions will have to include only the X, Y (no Z).
 WORKBENCH_VERSION = (5, 2, 47)
 
