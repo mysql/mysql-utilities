a188575f427f2f1c296cffaccbe4344a72ee651e
chuck.bell@oracle.com <>
2014-12-05 09:19:10 -0500

    Release-1.6.1 : fixed release type (alpha)

--- mysql/utilities/__init__.py
+++ mysql/utilities/__init__.py
@@ -18,7 +18,7 @@
 """mysql.utilities"""
 
 # Major, Minor, Patch, Status
-VERSION = (1, 6, 1, 'RC', 0)
+VERSION = (1, 6, 1, 'Alpha', 0)
 # Future versions will have to include only the X, Y (no Z).
 
 VERSION_STRING = "%s.%s.%s" % VERSION[0:3]
