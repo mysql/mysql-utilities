a158aae4d5986100539e1e4e5644ef736a133792
Nuno Mariz <nuno.mariz@oracle.com>
2015-07-23 16:37:39 +0100

    BUG#21415893 : mysqldbcompare fails when using --all option with server 5.7.7
    With MySQL server 5.7.7 the 'sys' schema is included by default. This
    was causing mysqldbcompare to fail when comparing all databases.
    
    No MUT test was changed or added, because existing ones already detected
    this issue.
    
    Note: Patch backported from release-1.5.5 (BZR)

--- mysql/utilities/command/dbcompare.py
+++ mysql/utilities/command/dbcompare.py
@@ -1,5 +1,5 @@
 #
-# Copyright (c) 2011, 2014, Oracle and/or its affiliates. All rights reserved.
+# Copyright (c) 2011, 2015, Oracle and/or its affiliates. All rights reserved.
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -531,6 +531,7 @@ def compare_all_databases(server1_val, server2_val, exclude_list, options):
         WHERE SCHEMA_NAME != 'INFORMATION_SCHEMA'
         AND SCHEMA_NAME != 'PERFORMANCE_SCHEMA'
         AND SCHEMA_NAME != 'mysql'
+        AND SCHEMA_NAME != 'sys'
         {0}"""
     conditions = ""
     if exclude_list:
